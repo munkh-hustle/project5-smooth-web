756463a1e288a28511475ec2de37c2c41c0b9319
munkh-hustle <93699468+munkh-hustle@users.noreply.github.com>
2023-03-23 13:10:42 +0800

    downloaded

--- smooth.js
+++ smooth.js
@@ -1,30 +1,45 @@
 const track = document.getElementById("image-track");
 
-window.onmousedown = e => {
-    track.dataset.mouseDownAt = e.clientX;
+const handleOnDown = e => track.dataset.mouseDownAt = e.clientX;
+
+const handleOnUp = () => {
+  track.dataset.mouseDownAt = "0";  
+  track.dataset.prevPercentage = track.dataset.percentage;
 }
-window.onmouseup = () => {
-    track.dataset.mouseDownAt = "0";
-    track.dataset.prevPercentage = track.dataset.percentage;
+
+const handleOnMove = e => {
+  if(track.dataset.mouseDownAt === "0") return;
+  
+  const mouseDelta = parseFloat(track.dataset.mouseDownAt) - e.clientX,
+        maxDelta = window.innerWidth / 2;
+  
+  const percentage = (mouseDelta / maxDelta) * -100,
+        nextPercentageUnconstrained = parseFloat(track.dataset.prevPercentage) + percentage,
+        nextPercentage = Math.max(Math.min(nextPercentageUnconstrained, 0), -100);
+  
+  track.dataset.percentage = nextPercentage;
+  
+  track.animate({
+    transform: `translate(${nextPercentage}%, -50%)`
+  }, { duration: 1200, fill: "forwards" });
+  
+  for(const image of track.getElementsByClassName("image")) {
+    image.animate({
+      objectPosition: `${100 + nextPercentage}% center`
+    }, { duration: 1200, fill: "forwards" });
+  }
 }
 
-window.onmousemove = e =>{
-    if(track.dataset.mouseDownAt==="0")return;
-    const mouseDelta = parseFloat(track.dataset.mouseDownAt)-e.clientX,maxDelta=window.innerWidth/2;
-    const percentage = (mouseDelta/maxDelta)*-100, nextPercentage = parseFloat(track.dataset.prevPercentage) +  percentage;
+/* -- Had to add extra lines for touch events -- */
 
-    track.dataset.percentage = nextPercentage;
+window.onmousedown = e => handleOnDown(e);
 
-    //track.style.transform = `translateX(${percentage}, -50%`;
-    track.animate({
-        transform: `translateX(${nextPercentage}, -50%)`
-    }, {duration: 1200, fill: "forwards"});
+window.ontouchstart = e => handleOnDown(e.touches[0]);
 
-    for(const image of track.getElementById("image")){
-        //image.style.objectPosition = `${nextPercentage + 100}% -50%`;
-        image.animate({
-            objectPosition: `${100 + nextPercentage}% center`
-        }, {duration: 1200, fill: "forwards"});
-    }
-    
-}
+window.onmouseup = e => handleOnUp(e);
+
+window.ontouchend = e => handleOnUp(e.touches[0]);
+
+window.onmousemove = e => handleOnMove(e);
+
+window.ontouchmove = e => handleOnMove(e.touches[0]);
