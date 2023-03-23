00e83af3fdf78d37af30b4828797bb15f532da34
munkh-hustle <93699468+munkh-hustle@users.noreply.github.com>
2023-03-23 12:54:34 +0800

    Add files via upload

---
+++ smooth.js
@@ -0,0 +1,30 @@
+const track = document.getElementById("image-track");
+
+window.onmousedown = e => {
+    track.dataset.mouseDownAt = e.clientX;
+}
+window.onmouseup = () => {
+    track.dataset.mouseDownAt = "0";
+    track.dataset.prevPercentage = track.dataset.percentage;
+}
+
+window.onmousemove = e =>{
+    if(track.dataset.mouseDownAt==="0")return;
+    const mouseDelta = parseFloat(track.dataset.mouseDownAt)-e.clientX,maxDelta=window.innerWidth/2;
+    const percentage = (mouseDelta/maxDelta)*-100, nextPercentage = parseFloat(track.dataset.prevPercentage) +  percentage;
+
+    track.dataset.percentage = nextPercentage;
+
+    //track.style.transform = `translateX(${percentage}, -50%`;
+    track.animate({
+        transform: `translateX(${nextPercentage}, -50%)`
+    }, {duration: 1200, fill: "forwards"});
+
+    for(const image of track.getElementById("image")){
+        //image.style.objectPosition = `${nextPercentage + 100}% -50%`;
+        image.animate({
+            objectPosition: `${100 + nextPercentage}% center`
+        }, {duration: 1200, fill: "forwards"});
+    }
+    
+}
